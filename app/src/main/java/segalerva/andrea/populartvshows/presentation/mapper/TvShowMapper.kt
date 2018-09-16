653c26e9b36cf0a739aa279632d01cf5c2dc18ee
Andrea Segalerva <andrea.segalerva@gmail.com>
2018-09-16 13:51:46 +0200

    Added tvShowMapper used in presentation layer

---
+++ app/src/main/java/segalerva/andrea/populartvshows/presentation/mapper/TvShowMapper.kt
@@ -0,0 +1,32 @@
+package segalerva.andrea.populartvshows.presentation.mapper
+
+import segalerva.andrea.populartvshows.domain.model.TvShow
+import segalerva.andrea.populartvshows.presentation.model.TvShowView
+
+/**
+ * Created by andrea on 16/9/18.
+ * Mapper to convert TvShow to TvShowView model which will be used when showing the movies in the view
+ */
+class TvShowMapper {
+
+    fun map(tvShow: TvShow): TvShowView {
+
+        return TvShowView(tvShow.originalName, tvShow.posterPath, tvShow.voteAverage, tvShow.originalLanguage)
+    }
+
+    fun mapList(tvShows: List<TvShow>): List<TvShowView> {
+
+        val tvShowViews = ArrayList<TvShowView>()
+
+        if (tvShows.isNotEmpty()) {
+
+            tvShows.mapTo(tvShowViews) {
+                map(it)
+            }
+        }
+
+        return tvShowViews;
+    }
+}
+
+
