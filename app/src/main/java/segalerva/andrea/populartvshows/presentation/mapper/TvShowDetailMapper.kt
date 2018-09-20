ff014deb4aa6e449a153c2549b744f3f71eb0119
Andrea Segalerva <andrea.segalerva@gmail.com>
2018-09-21 00:38:25 +0200

    Added cell similar tv show

--- app/src/main/java/segalerva/andrea/populartvshows/presentation/mapper/TvShowDetailMapper.kt
+++ app/src/main/java/segalerva/andrea/populartvshows/presentation/mapper/TvShowDetailMapper.kt
@@ -5,7 +5,10 @@ import segalerva.andrea.populartvshows.presentation.model.TvShowDetailView
 
 /**
  * Created by andrea on 20/9/18.
+ **Mapper to convert [TvShowDetail] to [TvShowDetailView] model used in the presentation layer
+ * @param tvShowMapper
  */
+
 class TvShowDetailMapper(private val tvShowMapper: TvShowMapper) {
 
     fun map(tvShowDetail: TvShowDetail): TvShowDetailView {
