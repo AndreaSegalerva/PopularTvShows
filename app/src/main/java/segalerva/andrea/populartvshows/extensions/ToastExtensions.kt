fa045fbe27fdfd19e46ac2f1df76d8d52ead80ae
Andrea Segalerva <andrea.segalerva@gmail.com>
2018-09-21 22:27:22 +0200

    Created base Toast customized

---
+++ app/src/main/java/segalerva/andrea/populartvshows/extensions/ToastExtensions.kt
@@ -0,0 +1,22 @@
+package segalerva.andrea.populartvshows.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import segalerva.andrea.populartvshows.R
+
+/**
+ * Created by andrea on 21/9/18.
+ *
+ */
+fun Toast.showToast(context: Context, message: String, gravity: Int, duration: Int) {
+    val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    val layout = inflater.inflate(R.layout.base_toast, (context as Activity).findViewById(R.id.base_toast_content))
+    layout.findViewById<TextView>(R.id.tv_message).text = message
+    setGravity(gravity, 0, 0)
+    setDuration(duration)
+    view = layout
+    show()
+}
